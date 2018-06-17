e3c079bdaae042404060a2445151ae87a0ba0dbf
KsuZZ <35704204+KsuZZ@users.noreply.github.com>
2018-06-17 10:55:13 +0300

    Update Process_multitasking.cpp

--- Process_multitasking.cpp
+++ Process_multitasking.cpp
@@ -8,6 +8,6 @@ using namespace std;
 void main() {
 	STARTUPINFO si = { sizeof(si) };
 	PROCESS_INFORMATION pi;
-	CreateProcess(TEXT("D:\\..path to the project..\\example.exe"), NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+	CreateProcess(TEXT("D:\\..path to the project..\\calculating.exe"), NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
 	system("pause");
 }
