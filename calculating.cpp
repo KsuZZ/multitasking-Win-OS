0181628b317ea885a6a3d75ab58dbf6818f34d96
KsuZZ <35704204+KsuZZ@users.noreply.github.com>
2018-06-17 10:42:38 +0300

    Create calculating.cpp
    example of calculating simple function

---
+++ calculating.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+int y, x;
+	while (true)
+	{
+for (x=0; x<11; x++)
+		{
+        y = x^3 + 2*x^2 + 3*x + 10;
+		cout<< y <<endl;
+		x = x - 0,99;
+		}
+	}
+}
