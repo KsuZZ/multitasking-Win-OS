329875c6e41809ca682969d68ce1ac64435b951d
KsuZZ <35704204+KsuZZ@users.noreply.github.com>
2018-06-17 10:48:28 +0300

    Create Process_multitasking.cpp

---
+++ Process_multitasking.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
+
+void main() {
+	STARTUPINFO si = { sizeof(si) };
+	PROCESS_INFORMATION pi;
+	CreateProcess(TEXT("D:\\..path to the project..\\example.exe"), NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+	system("pause");
+}
